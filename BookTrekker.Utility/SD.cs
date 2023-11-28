14b4e01e5576ae929a8d0f2ee1f1397679cb795e
Shailesh Shelar <shaileshshelar1217@gmail.com>
2023-11-28 15:25:18 +0530

    Order Management

--- BookTrekker.Utility/SD.cs
+++ BookTrekker.Utility/SD.cs
@@ -11,7 +11,7 @@ namespace BookTrekker.Utility
         public const string Role_Customer = "Customer";
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
-        public static string Role_Employee = "Employee";
+        public const string Role_Employee = "Employee";
 
 
         public const string StatusPending = "Pending";
